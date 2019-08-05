8b081e3b96ad3925f77f8ca55df2545ceb0e2584
Hoang Tran <trbhoang@gmail.com>
2019-08-05 23:04:59 +0700

    golang installation

--- provision.sh
+++ provision.sh
@@ -5,6 +5,7 @@
 
 TERRAFORM_VERSION="0.12.5"
 PACKER_VERSION="1.4.2"
+GOLANG_VERSION="1.12.7"
 WORKDIR=/tmp/provision
 
 mkdir -p $WORKDIR
@@ -20,13 +21,21 @@ sudo apt-get install ansible --yes
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 chmod +x terraform
-mv terraform /usr/local/bin
+sudo mv terraform /usr/local/bin
 
 # Packer installation
 wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
 unzip packer_${PACKER_VERSION}_linux_amd64.zip
 chmod +x packer
-mv packer /usr/local/bin
+sudo mv packer /usr/local/bin
+
+# Golang installation
+wget https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz
+mkdir $HOME/go
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
 
 # Environment variables
 # source /home/vagrant/provision.env
--- ssh-sync.sh
+++ ssh-sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# sync host's ssh config with guest's
+
 mkdir .ssh
 cp -vf ~/.ssh/* .ssh
 vagrant ssh -c "cp -rvf /vagrant/.ssh .; rm -rvf /vagrant/.ssh"
